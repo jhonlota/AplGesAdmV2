d4602cdfb44e3c144c3759e437f312331d38adb9
User <User@User-HP>
2017-03-22 01:58:36 -0500

    SUBIDA 22/03/2017 01:58 A.M.

--- src/clases/ClaseBaseDatos.java
+++ src/clases/ClaseBaseDatos.java
@@ -38,14 +38,14 @@ public class ClaseBaseDatos {
 //            ClaseGeneral.servidor =  array.get(5).substring(array.get(5).indexOf("='") + 2, array.get(5).indexOf("';"));
 
 /**///IMPORTANTE
-//            String url = "192.168.131.52";
-//            String dbname = "gest_ciencias";
-//            String user = "aga_ciencias";
-//            String pass = "AgA_CnS.hmcx36874";
-//            ClaseGeneral.servidor = "http://ingenieria.univalle.edu.co/facultadweb2/aga_ciencias/datos";
-//            ClaseGeneral.urlFTP = "192.168.131.52";
-//            ClaseGeneral.login = "aga_ciencias";
-//            ClaseGeneral.pwd = "AgA.hmcx36874";
+            String url = "192.168.131.52";
+            String dbname = "gest_ciencias";
+            String user = "aga_ciencias";
+            String pass = "AgA_CnS.hmcx36874";
+            ClaseGeneral.servidor = "http://ingenieria.univalle.edu.co/facultadweb2/aga_ciencias/datos";
+            ClaseGeneral.urlFTP = "192.168.131.52";
+            ClaseGeneral.login = "aga_ciencias";
+            ClaseGeneral.pwd = "AgA.hmcx36874";
             
 /**///IMPORTANTE
 //            String url = "192.168.131.52";
@@ -56,11 +56,6 @@ public class ClaseBaseDatos {
 //            ClaseGeneral.urlFTP = "192.168.131.52";
 //            ClaseGeneral.login = "aga_ingenieria";
 //            ClaseGeneral.pwd = "AgA_InG.Ktec871";
-
-            String url = "localhost";
-            String dbname = "gest_ciencias";
-            String user = "postgres";
-            String pass = "postgres";
 
             conexion = DriverManager.getConnection("jdbc:postgresql://" + url + ":5432/" + dbname, user, pass);
 //            System.out.println("jdbc:postgresql://" + url + ":5432/" + dbname + "," + user + "," + pass);
